25495a95ad3fe6bfa3dbbc4dd81546a10d1c7b56
iemong <kiyoshidainagon@gmail.com>
2021-03-01 23:46:18 +0900

    ログとして労働時間と生産性が出せるように

--- index.ts
+++ index.ts
@@ -1,4 +1,13 @@
+import * as Colors from "https://deno.land/std/fmt/colors.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
+
+type DateResult = {
+  id: number;
+  date: string;
+  productivity_pulse: number;
+  total_hours: number;
+  total_duration_formatted: string;
+};
 
 const key = config().API_KEY;
 const url = "https://www.rescuetime.com/anapi/daily_summary_feed";
@@ -8,4 +17,27 @@ const res = await fetch(`${url}?${queryParams}`);
 
 const json = await res.json();
 
-console.log(json);
+const filteredJson: DateResult[] = json.map((data: any) => {
+  const {
+    id,
+    date,
+    productivity_pulse,
+    total_hours,
+    total_duration_formatted,
+  } = data;
+  return {
+    id,
+    date,
+    productivity_pulse,
+    total_hours,
+    total_duration_formatted,
+  };
+});
+
+filteredJson.forEach((d) => {
+  console.log(
+    `[${
+      Colors.green(d.date)
+    }] 労働時間: ${d.total_duration_formatted} 生産性: ${d.productivity_pulse}`,
+  );
+});
